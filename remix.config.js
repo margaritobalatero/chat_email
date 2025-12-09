b3aaa45aa2c93a761744224a9e2e564f145e4066
margaritobalatero <margaritobalatero@gmail.com>
2025-12-09 23:14:58 +0800

    ok

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "production"
-    ? "./server-vercel.js"
-    : undefined,
-
-  appDirectory: "app",
-  ignoredRouteFiles: ["**/.*"]
+  serverBuildPath: "build/index.js",
+  server: "./server-vercel.js",
+  ignoredRouteFiles: ["**/.*"],
 };
+
